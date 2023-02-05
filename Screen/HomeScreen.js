a6a2fb2cd4d9ab53504912b729b983602d464768
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-05 18:08:12 +0530

    day-1

---
+++ Screen/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function HomeScreen() {
+  return (
+    <View>
+        <Text className='text-9xl'>HomeScreen</Text>
+        <Text>Kutubkhan</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
