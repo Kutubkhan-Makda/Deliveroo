7c75c224918c79a7131ec4d17b2f7c2ddca8821c
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-06 15:29:33 +0530

    day-2

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import React, { useLayoutEffect } from 'react'
 import { Text, View } from 'react-native'
 
 function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      
+    })
+  }, [])
+
   return (
     <View>
         <Text className='text-red-600'>HomeScreen</Text>
