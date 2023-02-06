6998999f685dcb695658f64c2d77ee2bb234fd3c
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-06 18:41:41 +0530

    day-2

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -12,12 +12,13 @@ function HomeScreen() {
   }, [])
 
   return (
-    <SafeAreaView>
-        <View>
+    <SafeAreaView className='p-2 pt-10'>
+        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
           <Image source={{uri:'https://links.papareact.com/wru'}} className='h-7 w-7 bg-gray-300 p-4 rounded-full'/>
-        </View>
-        <View>
-          <Text>Deliver Now</Text>
+          <View>
+            <Text className='font-bold text-gray-400 text-xs'>Deliver Now</Text>
+            <Text className='font-bold text-xl'>Currunt Location</Text>
+          </View>
         </View>
     </SafeAreaView>
   )
