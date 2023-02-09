1b4406f2d50126f6e79a61a8cc5554b76f97163b
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-09 21:29:02 +0530

    day-3

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import HomeScreen from './Screen/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
+});
--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native'
 import React, { useLayoutEffect } from 'react'
-import { Image, SafeAreaView, Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import {UserIcon,ChevronDownIcon,SearchIcon,AdjustmentsIcon} from 'react-native-heroicons/outline'
 
 function HomeScreen() {
   const navigation = useNavigation();
@@ -12,15 +13,17 @@ function HomeScreen() {
   }, [])
 
   return (
-    <SafeAreaView className='p-2 pt-10'>
-        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
-          <Image source={{uri:'https://links.papareact.com/wru'}} className='h-7 w-7 bg-gray-300 p-4 rounded-full'/>
-          <View>
-            <Text className='font-bold text-gray-400 text-xs'>Deliver Now</Text>
-            <Text className='font-bold text-xl'>Currunt Location</Text>
-          </View>
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image source={{uri:'https://links.papareact.com/wru'}} className='h-7 w-7 bg-gray-300 p-4 rounded-full'/>
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now</Text>
+          <Text className='font-bold text-xl'>
+            Currunt Location
+            <ChevronDownIcon size={20} color='#00CCBB'/>
+          </Text>
         </View>
-    </SafeAreaView>
+        <UserIcon size={35} color='#00CCBB'/>
+      </View>
   )
 }
 
