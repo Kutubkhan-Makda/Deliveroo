adbb6b397faa1ca3fe56eaf3353498b3f74bc448
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-16 19:55:03 +0530

    day-4

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import HomeScreen from './Screen/HomeScreen';
 
 const Stack = createNativeStackNavigator();
@@ -9,7 +8,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screen/HomeScreen.jsx
+++ Screen/HomeScreen.jsx
@@ -1,19 +1,10 @@
-import { useNavigation } from '@react-navigation/native'
-import React, { useLayoutEffect } from 'react'
+import React from 'react'
 import { Image, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native'
 import {UserIcon,ChevronDownIcon,SearchIcon,AdjustmentsIcon} from 'react-native-heroicons/outline'
 
-function HomeScreen() {
-  const navigation = useNavigation();
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown:false,
-    });
-  }, [])
-
+const HomeScreen = () => {
   return (
-      <SafeAreaView style={styles.AndroidSafeArea} className='bg-white pt-5'>
+    <SafeAreaView style={styles.AndroidSafeArea} className='bg-white pt-5'>
         <View className='flex-row pb-3 items-center mx-4 space-x-2'>
           <Image source={{uri:'https://links.papareact.com/wru'}} className='h-7 w-7 bg-gray-300 p-4 rounded-full'/>
           <View className='flex-1'>
@@ -26,8 +17,8 @@ function HomeScreen() {
           <UserIcon size={35} color='#00CCBB'/>
         </View>
         <View>
-          <View>
-            <SearchIcon/>
+          <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
+            <SearchIcon color='gray' size={20}/>
             <TextInput placeholder='Restaurants and cuisines' keyboardType='default'/>
           </View>
           <AdjustmentsIcon color='#00CCBB'/>
