346dedc42b49585a546b5575be857ba20206d225
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-17 17:36:25 +0530

    day-5

--- Components/Categories.jsx
+++ Components/Categories.jsx
@@ -5,10 +5,15 @@ import CategoryCard from './CategoryCard'
 const Categories = () => {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{paddingHorizontal:15,paddingTop:10}}>
-        <CategoryCard imgUrl='https://links.papareact.com/wru' title='testing'/>
-        <CategoryCard imgUrl='https://links.papareact.com/wru' title='testing'/>
-        <CategoryCard imgUrl='https://links.papareact.com/wru' title='testing'/>
-        <CategoryCard imgUrl='https://links.papareact.com/wru' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
+        <CategoryCard imgUrl='https://links.papareact.com/gn7' title='testing'/>
     </ScrollView>
   )
 }
