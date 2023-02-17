93f78cf9ac598a8a279aefa2d7c7359bb17e1035
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-17 16:08:13 +0530

    day-5

--- Screen/HomeScreen.jsx
+++ Screen/HomeScreen.jsx
@@ -5,7 +5,7 @@ import {UserIcon,ChevronDownIcon,MagnifyingGlassIcon,AdjustmentsVerticalIcon} fr
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.AndroidSafeArea} className='bg-white pt-5'>
-        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <View className='flex-row pb-3 items-center mx-4 space-x-2 px-2'>
           <Image source={{uri:'https://links.papareact.com/wru'}} className='h-7 w-7 bg-gray-300 p-4 rounded-full'/>
           <View className='flex-1'>
             <Text className='font-bold text-gray-400 text-xs'>Deliver Now</Text>
@@ -16,7 +16,7 @@ const HomeScreen = () => {
           </View>
           <UserIcon size={35} color='#00CCBB'/>
         </View>
-        <View>
+        <View className='flex-row items-center space-x-2 pb-2 mx-4'>
           <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
             <MagnifyingGlassIcon color='gray' size={20}/>
             <TextInput placeholder='Restaurants and cuisines' keyboardType='default'/>
