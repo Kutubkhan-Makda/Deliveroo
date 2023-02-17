33c69bc1a682da927aafb3c71f815a4837ab8873
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-17 23:56:49 +0530

    day-5

--- Components/FeaturedRows.jsx
+++ Components/FeaturedRows.jsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { ArrowRightIcon } from 'react-native-heroicons/outline'
 
 const FeaturedRows = ({id,title,description}) => {
   return (
     <View>
-      <Text>FeaturedRows</Text>
+      <View className='mt-4 flex-row items-center justify-between px-4'>
+        <Text className='font-bold text-lg'>{title}</Text>
+        <ArrowRightIcon color='#00CCBB'/>
+      </View>
     </View>
   )
 }
