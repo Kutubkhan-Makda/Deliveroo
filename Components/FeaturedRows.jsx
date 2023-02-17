a662309cc3675ffd02ddb95654408dc8f21f32ae
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-17 23:16:46 +0530

    day-5

--- Components/FeaturedRows.jsx
+++ Components/FeaturedRows.jsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const FeaturedRows = () => {
+const FeaturedRows = ({title,description,featuredCategory}) => {
   return (
     <View>
       <Text>FeaturedRows</Text>
