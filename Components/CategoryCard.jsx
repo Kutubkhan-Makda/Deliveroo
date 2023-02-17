7eaf29b03dc56daed30f454e22d67cdac3ba74fe
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-17 17:26:58 +0530

    day-5

--- Components/CategoryCard.jsx
+++ Components/CategoryCard.jsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const CategoryCard = ({imgUrl,title}) => {
   return (
-    <TouchableOpacity>
-      <Text>{title}</Text>
+    <TouchableOpacity className='relative mr-2'>
+        <Image source={{uri:imgUrl}} className='h-20 w-20 rounded'/>
+        <Text className='absolute bottom-1 left-1'>{title}</Text>
     </TouchableOpacity>
   )
 }
