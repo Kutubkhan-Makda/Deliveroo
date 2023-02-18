4579ad4b2f451a4f696e7d5bc5b426f640dc9057
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-18 10:34:29 +0530

    day-6

--- Sanity/schemas/index.ts
+++ Sanity/schemas/index.ts
@@ -1,6 +1,6 @@
 import blockContent from './blockContent'
 import category from './category'
-import post from './post'
 import author from './author'
+import restaurant from './restaurant'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [restaurant, author, category, blockContent]
