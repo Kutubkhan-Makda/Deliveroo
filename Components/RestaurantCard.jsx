e14864d1d655a347bf18bea328707d7f5b94de6b
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-18 09:47:22 +0530

    day-6

--- Components/RestaurantCard.jsx
+++ Components/RestaurantCard.jsx
@@ -1,7 +1,7 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { StarIcon } from "react-native-heroicons/solid";
-import {  } from "react-native-heroicons/outline";
+import { MapPinIcon } from "react-native-heroicons/outline";
 
 const RestaurantCard = ({id,imgUrl,title,rating,genre,address,short_description,dishes,long,lat}) => {
   return (
@@ -9,8 +9,14 @@ const RestaurantCard = ({id,imgUrl,title,rating,genre,address,short_description,
       <Image source={{uri:imgUrl}} className='h-36 w-64 rounded-sm'/>
       <View className='px-3 pb-4'>
         <Text className='font-bold text-lg pt-2'>{title}</Text>
+        <View className='flex-row items-center space-x-1'>
+          <StarIcon opacity={0.5} color='green' size={22}/>
+          <Text className='text-xs text-gray-500'>
+            <Text className='text-green-500'>{rating}</Text> · {genre}
+          </Text>
+        </View>
         <View>
-
+          <MapPinIcon/>
         </View>
       </View>
     </TouchableOpacity>
